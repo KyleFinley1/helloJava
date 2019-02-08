c00869e7b3a71f435827b649cc2293b3ab4f09f8
Kyle.Finley <Kyle.Finley@LinQuest.com>
2019-02-08 10:04:59 -0500

    Provided JAVA_HOME for Jenkins environment.

--- build.bat
+++ build.bat
@@ -1,2 +1,4 @@
-REM COPY helloJava.jav helloJava.java
-javac.exe helloJava.java
+set JAVA_HOME=C:\Program_Files_COTS\Java\jdk1.8.0_191\bin\
+
+REM javac.exe helloJava.java
+%JAVA_HOME%javac.exe helloJava.java
