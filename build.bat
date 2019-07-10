56f8aa801469be9079d658212e1274c48360709f
Kyle Finley <Kyle.Finley@LinQuest.com>
2019-07-10 12:55:48 -0400

    Corrected Java home path.

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-set JAVA_HOME=C:\Program_Files_COTS\Java\jdk1.8.0_191\bin\
+REM set JAVA_BIN=C:\Program_Files_COTS\Java\jdk1.8.0_151\bin\
 
 REM javac.exe helloJava.java
-%JAVA_HOME%javac.exe helloJava.java
+%JAVA_HOME%\\bin\\javac.exe helloJava.java
